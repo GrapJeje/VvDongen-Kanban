40e9b256b24860de1b1fa304e49a59832bf56c5e
Jason <jasonvanl2019@gmail.com>
2025-09-11 22:29:28 +0200

    Created models

---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['title', 'status', 'person'];
+
+    public function labels()
+    {
+        return $this->belongsToMany(Label::class, 'label_task')
+            ->withTimestamps();
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_task')
+            ->withTimestamps();
+    }
+}
